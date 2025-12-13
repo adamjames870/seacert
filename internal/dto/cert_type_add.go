ba33a5f0daba8ff753d63230884e40bc8aa549db
Adam James <adam.james870@yahoo.co.uk>
2025-12-13 20:25:53 +0000

    Minor bug fixes for postman runner

--- internal/dto/cert_type_add.go
+++ internal/dto/cert_type_add.go
@@ -2,7 +2,7 @@
 
 type ParamsAddCertificateType struct {
 	Name                 string `json:"name"`
-	ShortName            string `json:"short_name"`
-	StcwReference        string `json:"stcw_reference"`
-	NormalValidityMonths int32  `json:"normal_validity_months"`
+	ShortName            string `json:"short-name"`
+	StcwReference        string `json:"stcw-reference"`
+	NormalValidityMonths int32  `json:"normal-validity-months"`
 }
